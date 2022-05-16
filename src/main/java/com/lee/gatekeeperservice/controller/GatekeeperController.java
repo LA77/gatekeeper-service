a63b17044aac13cb22055a0bf275d5577af963f3
Lee Allen <leeallen_uk@yahoo.co.uk>
2022-05-16 20:44:56 +0100

    add code to get from external source and change port

--- src/main/java/com/lee/gatekeeperservice/controller/GatekeeperController.java
+++ src/main/java/com/lee/gatekeeperservice/controller/GatekeeperController.java
@@ -8,8 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,17 @@ public class GatekeeperController {
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> userList = userService.getAllUsers();
         return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    @GetMapping("ONS")
+    public void getONS() {
+        String url = "https://api.beta.ons.gov.uk/v1/datasets";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(url, String.class);
+        String productsJson = response.getBody();
+
+        System.out.println(productsJson);
     }
 
 }
